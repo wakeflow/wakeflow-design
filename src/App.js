49180e4e836414f2e46f2b0f8351d6ce6b0a4c91
Andreas <andi@wakeflow.io>
2021-12-06 13:30:14 +0000

    badge

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Badge from "./components/badge";
+
+function App() {
+  return (
+    <div className="App">
+      <Badge/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/badge.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Badge = () => {
+  return (
+    <Container>
+      <Text>5.12+</Text>
+    </Container>
+  )
+}
+
+export default Badge
+
+const Container = styled.div`
+  display:flex;
+  color:green;
+  background-color: lightgreen;
+  border-radius:40px;
+  padding:4px 8px;
+  width:50px;
+  text-align:center;
+  vertical-align:center;
+  align-items:center;
+  justify-content:center;
+`
+const Text = styled.div`
+`
