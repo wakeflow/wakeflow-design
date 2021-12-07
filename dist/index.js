4864412a1795f65ad63404627a154ca68a93fb97
Andreas <andi@wakeflow.io>
2021-12-07 10:59:19 +0000

    dist

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Badge", {
+  enumerable: true,
+  get: function get() {
+    return _badge.default;
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _button.default;
+  }
+});
+
+var _badge = _interopRequireDefault(require("./badge.js"));
+
+var _button = _interopRequireDefault(require("./button.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
